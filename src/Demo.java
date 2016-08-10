278901d4984f24fa7905a8d205bf49a07b3cf655
yangtaolnm <yangtao_lnm@163.com>
2016-08-10 13:12:46 +0800

    333

--- src/Demo.java
+++ src/Demo.java
@@ -3,7 +3,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("hello Git  2");
+        System.out.println("hello Git  3");
 	}
 
 }
