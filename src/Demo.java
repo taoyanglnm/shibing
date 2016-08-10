a0c2e352c1f503dbf5ccf3f5acc1ebb094923fe2
DELL <DELL@DELL-PC>
2016-08-10 15:05:16 +0800

    555

--- src/Demo.java
+++ src/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("hello Git  4");
-        System.out.println("haha  ni hao  xiaot");
+        System.out.println("haha  ni hao  xiaotu ");
 	}
 
 }
