0ff44f2048af550f996ba9e57e1477933398d1b3
taoyanglnm <yangtao_lnm@163.com>
2016-08-11 14:45:41 +0800

    6666

--- src/org/test/Demo.java
+++ src/org/test/Demo.java
@@ -1,3 +1,4 @@
+package org.test;
 
 public class Demo {
 
--- src/org/test/Demo2.java
+++ src/org/test/Demo2.java
@@ -1,9 +1,10 @@
+package org.test;
 
 public class Demo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("hello    ");
+        System.out.println("hello    xx");
 	}
 
 }
